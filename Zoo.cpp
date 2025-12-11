a46e6208b5340cd4917313b12ac9ba0281848c41
Nekokimi0 <75419315+Nekokimi0@users.noreply.github.com>
2025-12-11 21:12:48 +0100

    Update Zoo.cpp
    Suppression de "getBilletsParJour()" + Modification de la méthode "nombre_billets_jour()"

--- Zoo.cpp
+++ Zoo.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include <map>
+#include <cstdlib>
 #include "Zoo.hpp"
-#include "Animal.cpp"
-
 using namespace std;
 
 Zoo::Zoo() : date_actuelle(0) {}
@@ -11,21 +9,24 @@ void Zoo::ajouter_visiteur(const Visiteur& nouveau_visiteur){ // ajoute des visi
     visiteurs.push_back(nouveau_visiteur);
 };
 
+void Zoo::ajouter_enclos(const Enclos& e) {
+    enclos.push_back(e);
+};
+
 void Zoo::simuler_sante_animaux(){ //Donne aux animaux un état de santé aléatoire (en bonne santé ou pas) (true/false)
-    for (Enclos& e :enclos){
-         for (Animal& a : e.getAnimaux()) {
-            a.setSante(tirage_aleatoire());
+    for (auto& e : enclos){
+        for (auto& animal : e.getAnimaux()) {
+            bool tirage = (rand() % 100) < 95;
+            animal.changement_statut_sante(tirage);
         }
     }
-
 };
-    
 
 void Zoo::passer_jour(){ //passe au jour suivant de la simulation si certaines conditions sont remplies
-    for(const Enclos& e : enclos) {
-        for (const Animal& a : e.getAnimaux()) {
-            if (!a.getSante() || !a.getSatiete()) { // si les animaux sont nourris et en bonne santé
-                cout << "Vous ne pouvez pas passer au jour suivant, occupez vous de tous les animaux avant ! " << endl; // sinon affiche ce message
+    for(const auto& e : enclos) {
+        for (const auto& animal : e.getAnimaux()) {
+            if (!animal.getSante() || !animal.getSatiete()) { // si les animaux sont nourris et en bonne santé
+                cout << "Vous ne pouvez pas passer au jour suivant, occupez-vous de tous les animaux avant ! " << endl; // sinon affiche ce message
             }
         }
     }
@@ -34,9 +35,9 @@ void Zoo::passer_jour(){ //passe au jour suivant de la simulation si certaines c
     billets_par_jour[date_actuelle] = visiteurs; // prends ensuite en compte le nombre de billets vendus
     date_actuelle++; //augmente d'un jour la date
     visiteurs.clear(); //remet a 0 le nombre de visiteurs de la journée pour les stats
-    for (Enclos& e : enclos) {
-        for (Animal& a : e.getAnimaux()) {
-            a.setSatiete(false); // donne faim aux animaux
+    for (auto& e : enclos) {
+        for (auto& animal : e.getAnimaux()) {
+            animal.changement_statut_satiete(false); // donne faim aux animaux
         }
     }
 };
@@ -44,30 +45,31 @@ void Zoo::passer_jour(){ //passe au jour suivant de la simulation si certaines c
 int Zoo::nombre_billets_jour(int jour) const{ //calcule le nombre de billets vendus par jour
     int total = 0;
     auto it = billets_par_jour.find(jour);
-    if (it == billets_par_jour.end()) return 0;
+    if (it == billets_par_jour.end()){
+        return 0;
+    }
 
-    for (const Visiteur& v : it->second) {
-        total += v.getQuantiteBillets();
+    for (const auto& v : it->second) {
+        for (const auto& b : v.getBillets()) {
+            const string& type = b.first;
+            int quantite = b.second;
+            total += quantite;
+        }
     }
 
     return total;
-
 };
 
 int Zoo::benefice_jour(int jour) const{ //calcule les bénéfices réalisés par le parc pour une journée
     int total = 0;
     auto it = billets_par_jour.find(jour);
-    if (it == billets_par_jour.end()) return 0;
-
-    for (const Visiteur& v : it->second) {
-        total+= v.prix_total();
-
-        return total;
+    if (it == billets_par_jour.end()){
+        return 0;
     }
+
+    for (const auto& v : it->second) {
+        total += v.prix_total();
+    }
+
+    return total;
 };
-
-map<int, vector<Visiteur>> Zoo::getBilletsParJour() const{
-    return billets_par_jour;
-};
-
-
