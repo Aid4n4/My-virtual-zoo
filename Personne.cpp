83c188640e3279aaf619908e5b5bdca6fc67977d
Nekokimi0 <75419315+Nekokimi0@users.noreply.github.com>
2025-12-11 14:09:18 +0100

    Update Personne.cpp
    Ajout du "_" pour différencier les noms des attributs et les paramètres des constructeurs

--- Personne.cpp
+++ Personne.cpp
@@ -2,7 +2,7 @@
 #include "Personne.hpp"
 using namespace std;
 
-Personne::Personne(const string& nom, const string& prenom) : nom(nom), prenom(prenom) {};
+Personne::Personne(const string& _nom, const string& _prenom) : nom(_nom), prenom(_prenom) {};
 
 void Personne::afficher_informations() const {
     cout << "Nom : " << nom << "    Prénom : " << prenom << endl;
