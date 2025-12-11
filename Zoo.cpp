39b3d111ac61c6454b258a800d4cb29f281c7c89
Serena <serenapot@gmail.com>
2025-12-11 17:58:28 +0100

    remplissage fonctions Zoo.cpp + comments

--- Zoo.cpp
+++ Zoo.cpp
@@ -1,55 +1,70 @@
-/*Exercice 1, 2, 3
-+--------------------------------------+
-|               Zoo               |
-+--------------------------------------+
-| Attributs
-date_actuelle : int
-enclos : vector<Enclos>
-visiteurs : vector<Visiteur>
-billets_par_jour : map<int, vector<Visiteur>> → historique des visiteurs par jour => vérifie la structure de map + quelle librairie pour map
-Méthodes
-ajouter_visiteur : void → (Visiteur nouveau_visiteur)
-Ajouter nouveau_visiteur dans visiteurs
-simuler_sante_animaux : void → ()
-Parcours tous les animaux de tous les enclos
-Pour chaque animal : santé = tirage_aléatoire()
-passer_jour : void → ()
-Vérifier que tous les animaux dans tous les enclos ont : santé = true, satiété = true SINON ERREUR
-simuler_sante_animaux()
-billets_par_jour[date_actuelle, this] = visiteurs ; le this se réfère au visiteur pointé par l'instance de base, si ce n’est pas voulu, mettre a jour  
-incrémente date_actuelle
-visiteurs.clear()
-pour tous les enclos : pour tous les animaux : changement_statut_satiété(false)
-nombre_billets_jour : int → Visiteur : (int jour) 
-Parcours billets_par_jour[jour, this]
-Somme toutes les quantités de billets de chaque visiteur
-bénéfice_jour : int → (int jour)
-Parcours billets_par_jour[jour, this]
-Somme de visiteurs.prix_total() pour chaque visiteur
-Relations
-Zoo contient plusieurs Visiteur → agrégation
-Zoo contient plusieurs Enclos → agrégation         |
-+--------------------------------------+
-*/
-
 #include <iostream>
+#include <map>
 #include "Zoo.hpp"
+#include "Animal.cpp"
+
 using namespace std;
 
-Zoo::Zoo() : 
-void Zoo::ajouter_visiteur(const Visiteur& nouveau_visiteur){
-    Visiteur.push_back(nouveau_visiteur);
+Zoo::Zoo() : date_actuelle(0) {}
+
+void Zoo::ajouter_visiteur(const Visiteur& nouveau_visiteur){ // ajoute des visiteurs dans le parc
+    visiteurs.push_back(nouveau_visiteur);
 };
 
-void Zoo::simuler_sante_animaux();{
+void Zoo::simuler_sante_animaux(){ //Donne aux animaux un état de santé aléatoire (en bonne santé ou pas) (true/false)
+    for (Enclos& e :enclos){
+         for (Animal& a : e.getAnimaux()) {
+            a.setSante(tirage_aleatoire());
+        }
+    }
+
+};
     
-}
-void Zoo::passer_jour(){
-    if animaux
+
+void Zoo::passer_jour(){ //passe au jour suivant de la simulation si certaines conditions sont remplies
+    for(const Enclos& e : enclos) {
+        for (const Animal& a : e.getAnimaux()) {
+            if (!a.getSante() || !a.getSatiete()) { // si les animaux sont nourris et en bonne santé
+                cout << "Vous ne pouvez pas passer au jour suivant, occupez vous de tous les animaux avant ! " << endl; // sinon affiche ce message
+            }
+        }
+    }
+
+    simuler_sante_animaux(); 
+    billets_par_jour[date_actuelle] = visiteurs; // prends ensuite en compte le nombre de billets vendus
+    date_actuelle++; //augmente d'un jour la date
+    visiteurs.clear(); //remet a 0 le nombre de visiteurs de la journée pour les stats
+    for (Enclos& e : enclos) {
+        for (Animal& a : e.getAnimaux()) {
+            a.setSatiete(false); // donne faim aux animaux
+        }
+    }
 };
 
-int Zoo::nombre_billets_jour(int jour) const;
-int Zoo::benefice_jour(int jour) const;
+int Zoo::nombre_billets_jour(int jour) const{ //calcule le nombre de billets vendus par jour
+    int total = 0;
+    auto it = billets_par_jour.find(jour);
+    if (it == billets_par_jour.end()) return 0;
+
+    for (const Visiteur& v : it->second) {
+        total += v.getQuantiteBillets();
+    }
+
+    return total;
+
+};
+
+int Zoo::benefice_jour(int jour) const{ //calcule les bénéfices réalisés par le parc pour une journée
+    int total = 0;
+    auto it = billets_par_jour.find(jour);
+    if (it == billets_par_jour.end()) return 0;
+
+    for (const Visiteur& v : it->second) {
+        total+= v.prix_total();
+
+        return total;
+    }
+};
 
 map<int, vector<Visiteur>> Zoo::getBilletsParJour() const{
     return billets_par_jour;
