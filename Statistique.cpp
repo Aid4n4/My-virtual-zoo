79dcd2e431a86e4dcf70c5a5362e60b9a95be99c
Nekokimi0 <75419315+Nekokimi0@users.noreply.github.com>
2025-12-11 20:22:02 +0100

    Update Statistique.cpp
    Quelques modifications niveau écriture des méthodes

--- Statistique.cpp
+++ Statistique.cpp
@@ -1,40 +1,29 @@
 #include "Statistique.hpp"
-#include "Zoo.hpp"
-#include <map>
 
 using namespace std;
 
-Statistique::Statistique(Zoo* _zoo)
-    : zoo(_zoo)
-{}
+Statistique::Statistique(Zoo* _zoo) : zoo(_zoo) {};
 
-int Statistique::nombre_billets_totaux() const
-{
+int Statistique::nombre_billets_totaux() const {
     int total = 0;
 
-    const map<int, vector<Visiteur>>& historique = zoo->getBilletsParJour();
-
-    for (const auto& element : historique) {
-        int jour = element.first;
+    for (auto& paire : zoo->getBilletsParJour()) {
+        int jour = paire.first;
         total += zoo->nombre_billets_jour(jour);
     }
 
     return total;
-}
+};
 
-int Statistique::calculer_benefice_total() const
-{
+int Statistique::calculer_benefice_total() const {
     int total = 0;
 
-    const map<int, vector<Visiteur>>& historique = zoo->getBilletsParJour();
-
-    for (const auto& element : historique) {
-        int jour = element.first;
+    for (auto& paire : zoo->getBilletsParJour()) {
+        int jour = paire.first;
         total += zoo->benefice_jour(jour);
     }
 
     return total;
 }
-
 
 
