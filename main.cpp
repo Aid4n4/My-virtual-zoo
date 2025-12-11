52f5fb4392887f9a5973798464ec543338b2eb35
Nekokimi0 <75419315+Nekokimi0@users.noreply.github.com>
2025-12-11 21:38:36 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Zoo.hpp"
+#include "Enclos.hpp"
+#include "Animal.hpp"
+#include "Soigneur.hpp"
+#include "Visiteur.hpp"
+using namespace std;
+
+int main() {
+    cout << "-- Création du zoo --";
+    Zoo zoo;
+
+    Enclos enclos1(1, "Girafe", "Herbivore", "Plaine");
+    Enclos enclos2(2, "Lion", "Carnivore", "Savane");
+
+    zoo.ajouter_enclos(enclos1);
+    zoo.ajouter_enclos(enclos2);
+    
+    return 0;
+}
