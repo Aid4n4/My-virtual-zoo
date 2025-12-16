97c6a69c7dc72c262e004f31c98cbf8fe84180bb
Nekokimi0 <75419315+Nekokimi0@users.noreply.github.com>
2025-12-16 15:35:31 +0100

    Update Soigneur.hpp
    Enclos -> Soigneur : Issues #4

--- Soigneur.hpp
+++ Soigneur.hpp
@@ -11,14 +11,14 @@ private:
     Enclos* enclos_assigne;
 
 public:
-    Soigneur(const string& _nom, const string& _prenom, Enclos* _enclos_assigne);
+    Soigneur(const string& _nom, const string& _prenom);
     
+    void assigner_enclos(Enclos* _enclos_assigne);
     void verification_statut_tous() const;
     void remplir_nourriture_enclos(int date_actuelle);
     void soigner(Animal& animal);
 
     void afficher_informations() const override;
-
 };
 
 #endif
