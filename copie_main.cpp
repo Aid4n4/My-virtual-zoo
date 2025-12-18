4b28e3c63e67894427c16d352b49e8205a5198e5
Serena <serenapot@gmail.com>
2025-12-19 00:44:32 +0100

    Update copie_main.cpp
    L'utilisateur peut maintenant créer des enclos et y ajouter des animaux en leur donnant des noms

--- copie_main.cpp
+++ copie_main.cpp
@@ -7,110 +7,10 @@
 using namespace std;
 
 int main() {
-    /*srand(time(nullptr));
-
-    cout << "-- Creation du zoo --" << endl;
-    Zoo zoo("Chat");
-
-    cout << "-- Creation des enclos --" << endl;
-    zoo.ajouter_enclos(Enclos(1, "Girafe", "Herbivore", "Plaine"));
-    zoo.ajouter_enclos(Enclos (2, "Lion", "Carnivore", "Savane"));
-    
-    cout << "-- Creation des animaux --" << endl;
-    
-    zoo.getEnclos(0).ajout_animal(Animal (1, "Sophie"));
-    zoo.getEnclos(0).ajout_animal(Animal (2, "Cunegonde"));
-
-    Animal& girafe1 = zoo.getEnclos(0).getAnimaux()[0];
-    Animal& girafe2 = zoo.getEnclos(0).getAnimaux()[1];
-
-    girafe1.verification_statut();
-
-    zoo.getEnclos(1).ajout_animal(Animal (1, "Simba"));
-    zoo.getEnclos(1).ajout_animal(Animal (2, "Nala"));
-
-    Animal& lion1 = zoo.getEnclos(1).getAnimaux()[0];
-    Animal& lion2 = zoo.getEnclos(1).getAnimaux()[1];
-
-    cout << "Identifiant enclos : " << zoo.getEnclos(0).getID() << "\n Nombre d'animaux : " << zoo.getEnclos(0).getNombreAnimaux() << "\n Nourriture  : " << zoo.getEnclos(0).getNourriture() << endl;
-
-    cout << "Identifiant enclos : " << zoo.getEnclos(1).getID() << "\n Nombre d'animaux : " << zoo.getEnclos(1).getNombreAnimaux() << "\n Nourriture  : " << zoo.getEnclos(1).getNourriture() << endl;
-    
-    cout << "-- Creation des soigneurs --" << endl;
-    Soigneur soigneur1("Broillet", "Virgile");
-    Soigneur soigneur2("Quoi", "Feur");
-
-    soigneur1.assigner_enclos(&zoo.getEnclos(0));
-    soigneur2.assigner_enclos(&zoo.getEnclos(1));
-
-    soigneur1.afficher_informations();
-    soigneur2.afficher_informations();
-
-    soigneur1.verification_statut_tous();
-    soigneur1.remplir_nourriture_enclos(zoo.getDateActuelle());
-    soigneur2.remplir_nourriture_enclos(zoo.getDateActuelle());
-
-    cout << "Identifiant enclos : " << zoo.getEnclos(0).getID() << "\n Nombre d'animaux : " << zoo.getEnclos(0).getNombreAnimaux() << "\n Nourriture  : " << zoo.getEnclos(0).getNourriture() << endl;
-    
-    cout << "Identifiant enclos : " << zoo.getEnclos(1).getID() << "\n Nombre d'animaux : " << zoo.getEnclos(1).getNombreAnimaux() << "\n Nourriture  : " << zoo.getEnclos(1).getNourriture() << endl;
-
-    cout << "-- Creation des visiteurs --" << endl;
-    Visiteur visiteur1("Feur", "Nugget", 1 );
-    
-    visiteur1.acheter_billet("jeune (- de 18 ans)", 10);
-    visiteur1.acheter_billet("retraite (65 et +)", 1);
-    visiteur1.prix_total();
-
-    Visiteur visiteur2("Aubert", "Lily", 2);
-    visiteur2.acheter_billet("etudiant (18-25 ans)", 2);
-    visiteur2.prix_total();
-
-    zoo.ajouter_visiteur(visiteur1);
-    zoo.ajouter_visiteur(visiteur2);
-
-    cout << "-- Passer un jour --" << endl;
-    zoo.passer_jour();
-
-    cout << "-- Statistique du jour 1  --" << endl;
-    cout << "Nombre billets vendus jour 1 : " << zoo.nombre_billets_jour(zoo.getDernierJour()) << endl;
-    cout << "Benefice jour 1 : " << zoo.benefice_jour(zoo.getDernierJour()) << endl;
-
-    cout << "-- Jour 2 --" << endl;
-    soigneur1.verification_statut_tous();
-    soigneur2.verification_statut_tous();
-
-    soigneur1.soigner(girafe1);
-    soigneur1.soigner(girafe2);
-    soigneur2.soigner(lion1);
-    soigneur2.soigner(lion2);
-
-    soigneur1.remplir_nourriture_enclos(zoo.getDateActuelle());
-    soigneur2.remplir_nourriture_enclos(zoo.getDateActuelle());
-
-    soigneur1.verification_statut_tous();
-    soigneur2.verification_statut_tous();
-
-    cout << "-- Creation des visiteurs --" << endl;
-    Visiteur visiteur3("fghtj", "ghytj", 3);
-
-    visiteur3.acheter_billet("jeune (- de 18 ans)", 10);
-    visiteur3.acheter_billet("retraite (65 et +)", 1);
-    
-    zoo.ajouter_visiteur(visiteur3);
-
-    cout << "-- Passer un jour --" << endl;
-    zoo.passer_jour();
-
-    Statistique stats(&zoo);
-    cout << "Nombre billets vendus jour ALL : " << stats.nombre_billets_totaux() << endl;
-    cout << "Benefice jour ALL : " << stats.calculer_benefice_total() << endl;
-
-    return 0;*/
-
     srand(time(nullptr));
     string nom;
     cout << "-- Bienvenue dans My Virtual Zoo --" << endl;
-    cout << "Veuillez donner un nom a votre zoo : " << endl;
+    cout << " - Veuillez donner un nom a votre zoo : " << endl;
     cin >> nom;
     Zoo zoo(nom);
 
@@ -136,69 +36,119 @@ int main() {
         {"Lynx", {"Carnivore", "Foret"}},
         {"Cobra", {"Carnivore", "Vivarium"}},
         {"Panda Roux", {"Omnivore", "Foret"}},
-        {"Flamant Rose", {"Carnivore", "Lac"}},
-        
+        {"Flamant Rose", {"Carnivore", "Lac"}},   
     };
-    cout << "- Choisis quel animal tu veux mettre à l'intérieur"<< endl;
-    cout << "- Tu as le choix entre tout ces animaux : \n" << "Girafe, Lion, Pingouin, Loup, Guepard, Koala, Tigre, Rhinoceros, Dauphin, Tortue terrestre, Tortue aquatique, Ouistiti, Gorille, Perroquet, Gazelle, Crocodile, Lynx, Cobra, Panda Roux, Flamant Rose. " << endl;
+        
+    vector<string> noms_animaux;
+    for (const auto& a : animaux) {
+        noms_animaux.push_back(a.first);
+    }
 
-    /* zoo.ajouter_enclos(Enclos(1, "Girafe", "Herbivore", "Plaine"));
-    zoo.ajouter_enclos(Enclos (2, "Lion", "Carnivore", "Savane"));
+    int nb_enclos = 0;
+
+    char continuer = 'o';
+    int id_enclos = 1;
+
+    while (continuer == 'o' || continuer == 'O') {
+        cout << "\n--- Creation d'un enclos ---\n";
+        cout << "- Choisis un animal :\n";
+
+        for (size_t i = 0; i < noms_animaux.size(); i++) {
+            cout << i + 1 << " - " << noms_animaux[i] << endl;
+        }
+        int choix;
+        cin >> choix;
+        if (choix < 1 || choix > noms_animaux.size()) {
+            cout << "- Choix invalide.\n";
+            continue;
+        }
+
+        string nom_animal = noms_animaux[choix - 1];
+        string regime = animaux[nom_animal][0];
+        string habitat = animaux[nom_animal][1];
+
+        zoo.ajouter_enclos(Enclos(id_enclos++, nom_animal, regime, habitat));
+        nb_enclos++;
+        cout << "- Enclos pour " << nom_animal << " cree avec succes !\n";
+
+        cout << "- Souhaites-tu creer un autre enclos ? (o/n) : " << endl;
+        cin >> continuer;
+    }
     
-    cout << "- Super ! Maintenant que nous avons leurs habitats il faudrait ajouter des animaux." << endl;
-    
-    zoo.getEnclos(0).ajout_animal(Animal (1, "Sophie"));
-    zoo.getEnclos(0).ajout_animal(Animal (2, "Cunegonde"));
+    cout << "- Maintenant que nous avons les enclos il faudrait les remplir" << endl;
 
-    Animal& girafe1 = zoo.getEnclos(0).getAnimaux()[0];
-    Animal& girafe2 = zoo.getEnclos(0).getAnimaux()[1];
+    cout << "\n--- Ajout des animaux dans les enclos ---\n";
 
-    girafe1.verification_statut();
+    for (int i = 0; i < nb_enclos; i++) {
 
-    zoo.getEnclos(1).ajout_animal(Animal (1, "Simba"));
-    zoo.getEnclos(1).ajout_animal(Animal (2, "Nala"));
+        Enclos& enclos = zoo.getEnclos(i);
 
-    Animal& lion1 = zoo.getEnclos(1).getAnimaux()[0];
-    Animal& lion2 = zoo.getEnclos(1).getAnimaux()[1];
+        cout << "\nEnclos numéro : " << enclos.getID() << endl;
 
-    cout << "Identifiant enclos : " << zoo.getEnclos(0).getID() << "\n Nombre d'animaux : " << zoo.getEnclos(0).getNombreAnimaux() << "\n Nourriture  : " << zoo.getEnclos(0).getNourriture() << endl;
+        int nb_animaux;
+        cout << "- Combien d'animaux veux-tu ajouter dans cet enclos ? ";
+        cin >> nb_animaux;
 
-    cout << "Identifiant enclos : " << zoo.getEnclos(1).getID() << "\n Nombre d'animaux : " << zoo.getEnclos(1).getNombreAnimaux() << "\n Nourriture  : " << zoo.getEnclos(1).getNourriture() << endl;
-    
-    cout << "-- Creation des soigneurs --" << endl;
-    Soigneur soigneur1("Broillet", "Virgile", &zoo.getEnclos(0));
-    Soigneur soigneur2("Quoi", "Feur", &zoo.getEnclos(1));
+        if (!cin || nb_animaux <= 0) {
+            cin.clear();
+            cin.ignore(10000, '\n');
+            cout << "- Nombre invalide, passons a l'enclos suivant.\n";
+            continue;
+    }
 
-    soigneur1.verification_statut_tous();
+    for (int j = 0; j < nb_animaux; j++) {
+
+        string nom_animal;
+        cout << "Nom de l'animal numéro" << j + 1 << " de ton enclos : ";
+        cin >> nom_animal;
+
+        int id_animal = j + 1;
+
+        enclos.ajout_animal(Animal(id_animal, nom_animal));
+    }
+}
+
+}
+/*
+    cout << "\n-- Creation des soigneurs --" << endl;
+    Soigneur soigneur1("Broillet", "Virgile");
+    Soigneur soigneur2("Quoi", "Feur");
+
+    soigneur1.assigner_enclos(&zoo.getEnclos(0));
+    soigneur2.assigner_enclos(&zoo.getEnclos(1));
+
+    zoo.ajouter_soigneur(soigneur1);
+    zoo.ajouter_soigneur(soigneur2);
+
+    soigneur1.afficher_informations();
+    soigneur2.afficher_informations();
+
+    zoo.getEnclos(0).afficher_informations();
+    zoo.getEnclos(1).afficher_informations();
+
     soigneur1.remplir_nourriture_enclos(zoo.getDateActuelle());
     soigneur2.remplir_nourriture_enclos(zoo.getDateActuelle());
 
-    cout << "Identifiant enclos : " << zoo.getEnclos(0).getID() << "\n Nombre d'animaux : " << zoo.getEnclos(0).getNombreAnimaux() << "\n Nourriture  : " << zoo.getEnclos(0).getNourriture() << endl;
-
-   cout << "Identifiant enclos : " << zoo.getEnclos(1).getID() << "\n Nombre d'animaux : " << zoo.getEnclos(1).getNombreAnimaux() << "\n Nourriture  : " << zoo.getEnclos(1).getNourriture() << endl;
-
-    cout << "-- Creation des visiteurs --" << endl;
+    cout << "\n-- Creation des visiteurs --" << endl;
     Visiteur visiteur1("Feur", "Nugget", 1 );
     
     visiteur1.acheter_billet("jeune (- de 18 ans)", 10);
     visiteur1.acheter_billet("retraite (65 et +)", 1);
-    visiteur1.prix_total();
 
     Visiteur visiteur2("Aubert", "Lily", 2);
     visiteur2.acheter_billet("etudiant (18-25 ans)", 2);
-    visiteur2.prix_total();
 
     zoo.ajouter_visiteur(visiteur1);
     zoo.ajouter_visiteur(visiteur2);
 
-    cout << "-- Passer un jour --" << endl;
+    cout << "\n-- Passer un jour --" << endl;
     zoo.passer_jour();
 
-    cout << "-- Statistique du jour 1  --" << endl;
+    cout << "\n-- Statistique du jour 1  --" << endl;
     cout << "Nombre billets vendus jour 1 : " << zoo.nombre_billets_jour(zoo.getDernierJour()) << endl;
     cout << "Benefice jour 1 : " << zoo.benefice_jour(zoo.getDernierJour()) << endl;
 
-    cout << "-- Jour 2 --" << endl;
+    cout << "\n-- Jour 2 --" << endl;
     soigneur1.verification_statut_tous();
     soigneur2.verification_statut_tous();
 
@@ -213,7 +163,7 @@ int main() {
     soigneur1.verification_statut_tous();
     soigneur2.verification_statut_tous();
 
-    cout << "-- Creation des visiteurs --" << endl;
+    cout << "\n-- Creation des visiteurs --" << endl;
     Visiteur visiteur3("fghtj", "ghytj", 3);
 
     visiteur3.acheter_billet("jeune (- de 18 ans)", 10);
@@ -221,12 +171,11 @@ int main() {
     
     zoo.ajouter_visiteur(visiteur3);
 
-    cout << "-- Passer un jour --" << endl;
+    cout << "\n-- Passer un jour --" << endl;
     zoo.passer_jour();
 
-    Statistique stats(&zoo);
+    Statistique stats(zoo);
     cout << "Nombre billets vendus jour ALL : " << stats.nombre_billets_totaux() << endl;
-    cout << "Benefice jour ALL : " << stats.calculer_benefice_total() << endl; */
+    cout << "Benefice jour ALL : " << stats.calculer_benefice_total() << endl;
+    return 0;*/
 
-    return 0;
-}
