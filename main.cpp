abffb00aab482645935d4bfd1946dc8d0ebe4b25
Nekokimi0 <75419315+Nekokimi0@users.noreply.github.com>
2025-12-18 20:48:02 +0100

    Update main.cpp
    Mise en forme corrigé

--- main.cpp
+++ main.cpp
@@ -12,11 +12,13 @@ int main() {
     cout << "-- Creation du zoo --" << endl;
     Zoo zoo("Chat");
 
-    cout << "-- Creation des enclos --" << endl;
+
+
+    cout << "\n-- Creation des enclos --" << endl;
     zoo.ajouter_enclos(Enclos(1, "Girafe", "Herbivore", "Plaine"));
     zoo.ajouter_enclos(Enclos (2, "Lion", "Carnivore", "Savane"));
     
-    cout << "-- Creation des animaux --" << endl;
+    cout << "\n-- Creation des animaux --" << endl;
     
     zoo.getEnclos(0).ajout_animal(Animal (1, "Sophie"));
     zoo.getEnclos(0).ajout_animal(Animal (2, "Cunegonde"));
@@ -24,19 +26,13 @@ int main() {
     Animal& girafe1 = zoo.getEnclos(0).getAnimaux()[0];
     Animal& girafe2 = zoo.getEnclos(0).getAnimaux()[1];
 
-    girafe1.verification_statut();
-
     zoo.getEnclos(1).ajout_animal(Animal (1, "Simba"));
     zoo.getEnclos(1).ajout_animal(Animal (2, "Nala"));
 
     Animal& lion1 = zoo.getEnclos(1).getAnimaux()[0];
     Animal& lion2 = zoo.getEnclos(1).getAnimaux()[1];
 
-    cout << "Identifiant enclos : " << zoo.getEnclos(0).getID() << "\n Nombre d'animaux : " << zoo.getEnclos(0).getNombreAnimaux() << "\n Nourriture  : " << zoo.getEnclos(0).getNourriture() << endl;
-
-    cout << "Identifiant enclos : " << zoo.getEnclos(1).getID() << "\n Nombre d'animaux : " << zoo.getEnclos(1).getNombreAnimaux() << "\n Nourriture  : " << zoo.getEnclos(1).getNourriture() << endl;
-    
-    cout << "-- Creation des soigneurs --" << endl;
+    cout << "\n-- Creation des soigneurs --" << endl;
     Soigneur soigneur1("Broillet", "Virgile");
     Soigneur soigneur2("Quoi", "Feur");
 
@@ -49,15 +45,13 @@ int main() {
     soigneur1.afficher_informations();
     soigneur2.afficher_informations();
 
-    soigneur1.verification_statut_tous();
+    zoo.getEnclos(0).afficher_informations();
+    zoo.getEnclos(1).afficher_informations();
+
     soigneur1.remplir_nourriture_enclos(zoo.getDateActuelle());
     soigneur2.remplir_nourriture_enclos(zoo.getDateActuelle());
 
-    cout << "Identifiant enclos : " << zoo.getEnclos(0).getID() << "\n Nombre d'animaux : " << zoo.getEnclos(0).getNombreAnimaux() << "\n Nourriture  : " << zoo.getEnclos(0).getNourriture() << endl;
-    
-    cout << "Identifiant enclos : " << zoo.getEnclos(1).getID() << "\n Nombre d'animaux : " << zoo.getEnclos(1).getNombreAnimaux() << "\n Nourriture  : " << zoo.getEnclos(1).getNourriture() << endl;
-
-    cout << "-- Creation des visiteurs --" << endl;
+    cout << "\n-- Creation des visiteurs --" << endl;
     Visiteur visiteur1("Feur", "Nugget", 1 );
     
     visiteur1.acheter_billet("jeune (- de 18 ans)", 10);
@@ -71,14 +65,14 @@ int main() {
     zoo.ajouter_visiteur(visiteur1);
     zoo.ajouter_visiteur(visiteur2);
 
-    cout << "-- Passer un jour --" << endl;
+    cout << "\n-- Passer un jour --" << endl;
     zoo.passer_jour();
 
-    cout << "-- Statistique du jour 1  --" << endl;
+    cout << "\n-- Statistique du jour 1  --" << endl;
     cout << "Nombre billets vendus jour 1 : " << zoo.nombre_billets_jour(zoo.getDernierJour()) << endl;
     cout << "Benefice jour 1 : " << zoo.benefice_jour(zoo.getDernierJour()) << endl;
 
-    cout << "-- Jour 2 --" << endl;
+    cout << "\n-- Jour 2 --" << endl;
     soigneur1.verification_statut_tous();
     soigneur2.verification_statut_tous();
 
@@ -93,7 +87,7 @@ int main() {
     soigneur1.verification_statut_tous();
     soigneur2.verification_statut_tous();
 
-    cout << "-- Creation des visiteurs --" << endl;
+    cout << "\n-- Creation des visiteurs --" << endl;
     Visiteur visiteur3("fghtj", "ghytj", 3);
 
     visiteur3.acheter_billet("jeune (- de 18 ans)", 10);
@@ -101,10 +95,10 @@ int main() {
     
     zoo.ajouter_visiteur(visiteur3);
 
-    cout << "-- Passer un jour --" << endl;
+    cout << "\n-- Passer un jour --" << endl;
     zoo.passer_jour();
 
-    Statistique stats(&zoo);
+    Statistique stats(zoo);
     cout << "Nombre billets vendus jour ALL : " << stats.nombre_billets_totaux() << endl;
     cout << "Benefice jour ALL : " << stats.calculer_benefice_total() << endl;
 
