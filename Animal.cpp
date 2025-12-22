df794c6b8aac5812445de78e21c31fd9e66d82e8
Nekokimi0 <75419315+Nekokimi0@users.noreply.github.com>
2025-12-22 20:30:14 +0100

    Update Animal.cpp
    Issues #10 + Issues #16

--- Animal.cpp
+++ Animal.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include "Animal.hpp"
+#include "Utilitaires.hpp"
 using namespace std;
 
 Animal::Animal(int _numero, const string& _nom) : numero(_numero), nom(_nom), sante(true), satiete(false), derniere_date_nourri(0) {};
 
 void Animal::verification_statut() const{
-    cout << "Animal #" << numero << " (" << nom << ")\n" 
-    << "    Sante : " << (sante ? "Bonne" : "Mauvaise") << "\n" 
-    << "    Satiete : " << (satiete ? "Rassasie" : "Affame") << "\n" 
-    << "    Derniere date nourri : Jour " << derniere_date_nourri << endl;
+    cout << "Animal #" << numero << " (" << nom << ")" << endl;
+    pause(1);
+    cout << "- Sante : " << (sante ? "Bonne" : "Mauvaise") << endl;
+    cout << "- Satiete : " << (satiete ? "Rassasie" : "Affame") << endl;
+    cout << "- Derniere date nourri : Jour " << derniere_date_nourri << endl;
 };
 
 void Animal::changement_statut_sante(bool nouvel_etat){
