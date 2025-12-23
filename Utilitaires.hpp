b686e8a20eaef4294ecca02fe4ddc5304d978920
Nekokimi0 <75419315+Nekokimi0@users.noreply.github.com>
2025-12-23 17:50:38 +0100

    Update Utilitaires.hpp

--- Utilitaires.hpp
+++ Utilitaires.hpp
@@ -1,6 +1,15 @@
 #ifndef UTILITAIRES_HPP
 #define UTILITAIRES_HPP
 
+/**
+ * @brief Met le programme en pause pendant un certain nombre de secondes.
+ *
+ * Cette fonction est utilisée pour ralentir l'affichage du programme
+ * afin d'améliorer la lisibilité des informations affichées à l'écran
+ * (simulation temporelle).
+ *
+ * @param secondes : Durée de la pause en secondes
+ */
 void pause(int secondes);
 
 #endif
