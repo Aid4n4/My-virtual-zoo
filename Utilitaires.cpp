ffc439f0caf90be3a438d318cd594f740ce5667e
Nekokimi0 <75419315+Nekokimi0@users.noreply.github.com>
2025-12-23 17:50:49 +0100

    Enhance pause function with comments
    Added comments to clarify the pause function's behavior.

--- Utilitaires.cpp
+++ Utilitaires.cpp
@@ -2,7 +2,10 @@
 #include <ctime>
 using namespace std;
 
+// Bloque l'exécution du programme pendant un nombre donné de secondes
 void pause(int secondes) {
     time_t debut = time(nullptr);
+
+    // Attente active jusqu'à ce que le temps écoulé atteigne la durée demandée
     while (difftime(time(nullptr), debut) < secondes) {}
 };
