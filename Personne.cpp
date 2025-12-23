ea0e3af4a0255879f3faad02f4d9ad8b8941d53c
Nekokimi0 <75419315+Nekokimi0@users.noreply.github.com>
2025-12-23 17:40:19 +0100

    Update Personne.cpp

--- Personne.cpp
+++ Personne.cpp
@@ -2,8 +2,10 @@
 #include "Personne.hpp"
 using namespace std;
 
+// Initialise le nom et le prénom de la personne
 Personne::Personne(const string& _nom, const string& _prenom) : nom(_nom), prenom(_prenom) {};
 
+// Affiche les informations de base de la personne
 void Personne::afficher_informations() const {
     cout << "- Nom : " << nom << endl;
     cout << "- Prenom : " << prenom << endl;
